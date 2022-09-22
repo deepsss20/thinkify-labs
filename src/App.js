2c5a04eac14f5f3c19542f667c4abf9e4b3934b8
unknown <hvdeepali@gmail.com>
2022-09-22 08:39:17 +0530

    developed new feture

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-// import {useGetEmailListQuery} from "./services/email.services"
 import { Route, Routes } from "react-router-dom";
 import MailBox from "./pages/MailBox/MailBox";
 import React,{Fragment} from "react";
 
 function App() {
-  // const{data, error, isLoading, isSuccess, isError} = useGetEmailListQuery()
-  // console.log(data)
   return (
     <div className="App">
       <Routes>
